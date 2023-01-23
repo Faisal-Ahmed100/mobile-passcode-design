9a86f97d0dc1d6e98974e50b76086eb59d4cf59d
Faisal <princepavel100@gmail.ocm>
2023-01-23 07:08:20 +0600

    all issue solve

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 var count = 0;
 var circleCount = [];
+var error = false;
 
 $(".digit").on("click", function () {
+  console.log(error);
   var num = $(this).clone().children().remove().end().text();
   if (count < 6) {
     $("#circles span.circleOutline:nth-child(" + (count + 1) + ")").addClass(
       "active"
     );
+    error = false;
+    if (!error) {
+      $(".passcode-title-wrapper").css("display", "block");
+      $("#errorPasscode").css("display", "none");
+      $(".backspace").css("opacity", 1);
+      $("#circles").removeClass("error");
+    }
 
     count++;
   }
+  if (count > 5) {
+    error = true;
+    if (error) {
+      $(".passcode-title-wrapper").css("display", "none");
+      $("#errorPasscode").css("display", "block");
+      $(".backspace").css("opacity", 0);
+      $("#circles").addClass("error");
+
+      // removing all filled background of circle
+      $("#circles span.circleOutline").removeClass("active");
+    }
+
+    count = 0;
+  }
 });
+
 // back space js
 $(".backspace").on("click", function () {
   if (count > 0) {
